9dd87bca3850f18d565e92dbe1a95e8912dbfc4d
EC2 Default User <ec2-user@ip-172-31-20-147.ec2.internal>
2021-01-14 12:09:46 +0000

    [Add] image_id to lists

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_13_012250) do
+ActiveRecord::Schema.define(version: 2021_01_14_120744) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_id"
   end
 
 end
