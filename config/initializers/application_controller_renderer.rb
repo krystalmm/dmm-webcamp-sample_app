7aaec1538d9954c07b62e1efa6c9771f9a98d327
EC2 Default User <ec2-user@ip-172-31-20-147.ec2.internal>
2021-01-14 12:47:13 +0000

    [Finish] 2-7

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+# 画像投稿のシークレットキー
+Refile.secret_key = '0c3732de3cab01954edd0a8c9e6b571e7eab251a067e9c21331e4047b3e8a532d420c53bfb5596878372067df1f9f51d6a940ce455b3c06d689ba2e09355de28'
