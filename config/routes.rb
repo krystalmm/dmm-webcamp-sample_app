6c6410500a0de13354c0ad77d010dce91e171884
EC2 Default User <ec2-user@ip-172-31-20-147.ec2.internal>
2021-01-14 11:54:47 +0000

    [Add] edit and update action to todolists

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   
   get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
   
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+  
 end
 
